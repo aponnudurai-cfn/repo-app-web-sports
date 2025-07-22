a4b90f62a8b9248d4c7ba484d236b4875a3fa6ca
aponnudurai-cfn <164194534+aponnudurai-cfn@users.noreply.github.com>
2025-07-21 20:07:15 -0400

    Update resource2.ts

--- amplify/data/resource2.ts
+++ amplify/data/resource2.ts
@@ -1,36 +1,36 @@
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
-import { Collection } from "@aws-amplify/ui-react";
+import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any user authenticated via an API key can "create", "read",
-"update", and "delete" any "Todo" records.
-=========================================================================*/
-const schema = a.schema({
-  Customer: a
-    .model({
-      customerId: a.id().required(),
-      name: a.string(),
-      location: a.customType({
-        lat: a.float().required(),
-        long: a.float().required(),
-      }),
-      engagementStage: a.enum(["PROSPECT", "INTERESTED", "PURCHASED"]),
-      collectionId: a.id(),
-      collection: a.belongsTo("Collection","collectionId"),
-    })
-    .identifier(["customerId"]),
-  Collection: a
-    .model({
-      customers: a.hasMany("Customer", "collectionId"),
-      tags: a.string().array(),
-      representativeId: a.id().required(),
-    })
-    .secondaryIndexes((index) => [index("representativeId")])
-}).authorization((allow) => {
-  return [allow.publicApiKey()];
-});
+const schema = a
+  .schema({
+    Customer: a
+      .model({
+        customerId: a.id().required(),
+        // fields can be of various scalar types,
+        // such as string, boolean, float, integers etc.
+        name: a.string(),
+        // fields can be of custom types
+        location: a.customType({
+          // fields can be required or optional
+          lat: a.float().required(),
+          long: a.float().required(),
+        }),
+        // fields can be enums
+        engagementStage: a.enum(["PROSPECT", "INTERESTED", "PURCHASED"]),
+        collectionId: a.id(),
+        collection: a.belongsTo("Collection", "collectionId")
+        // Use custom identifiers. By default, it uses an `id: a.id()` field
+      })
+      .identifier(["customerId"]),
+    Collection: a
+      .model({
+        customers: a.hasMany("Customer", "collectionId"), // setup relationships between types
+        tags: a.string().array(), // fields can be arrays
+        representativeId: a.id().required(),
+        // customize secondary indexes to optimize your query performance
+      })
+      .secondaryIndexes((index) => [index("representativeId")]),
+  })
+  .authorization((allow) => [allow.publicApiKey()]);
 
 export type Schema = ClientSchema<typeof schema>;
 
@@ -38,38 +38,8 @@ export const data = defineData({
   schema,
   authorizationModes: {
     defaultAuthorizationMode: "apiKey",
-    // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
     },
   },
 });
-
-/*== STEP 2 ===============================================================
-Go to your frontend source code. From your client-side code, generate a
-Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
-WORK IN THE FRONTEND CODE FILE.)
-
-Using JavaScript or Next.js React Server Components, Middleware, Server 
-Actions or Pages Router? Review how to generate Data clients for those use
-cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
-=========================================================================*/
-
-/*
-"use client"
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-
-const client = generateClient<Schema>() // use this Data client for CRUDL requests
-*/
-
-/*== STEP 3 ===============================================================
-Fetch records from the database and use them in your frontend component.
-(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
-=========================================================================*/
-
-/* For example, in a React component, you can use this snippet in your
-  function's RETURN statement */
-// const { data: todos } = await client.models.Todo.list()
-
-// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
